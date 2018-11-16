54f57122ad278b316f392c3498c17f35834e24e5
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2018-11-16 12:19:38 -0500

    some tests

--- test/rubin2017genbio.jl
+++
@@ -1,4 +0,0 @@
-@testset "rubin2017genbio" begin
-    data = PhageData.rubin2017genbio()
-    @test allunique(data.sequences)
-end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,12 @@
 using Test, PhageData, PhageBase
 
-#include("rubin2017.jl")
+
+@testset "rubin2017genbio" begin
+    data = PhageData.rubin2017genbio()
+    @test allunique(data.sequences)
+end
+
+@testset "boyer2016pnas" begin
+    data = PhageData.boyer2016pnas_pvp()
+    @test allunique(data.sequences)
+end
