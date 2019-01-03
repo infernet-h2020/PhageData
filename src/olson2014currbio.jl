7b2d521a8bf334099ca49e7940894254a9870561
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2019-01-03 14:49:46 -0500

    simplify sha

--- src/olson2014currbio.jl
+++ src/olson2014currbio.jl
@@ -70,10 +70,7 @@ function olson2014currbio_download()
     url = "https://www.cell.com/cms/10.1016/j.cub.2014.09.072/attachment/3a36211d-bddd-43e3-bf42-a6721f93a18b/mmc2.xlsx"
     file_path = olson2014currbio_dir * "/mmc2.xlsx"
     run(`wget -o $file_path $url`)
-    s = open(file_path) do f
-        bytes2hex(sha2_256(f))
-    end
-    if s ≠ "e912c93cf3d1d95a7d8fc5bb8e8d50a523eb9398c51613b2a74693f07a50fe29"
+    if bytes2hex(open(sha2_256, file_path)) ≠ "e912c93cf3d1d95a7d8fc5bb8e8d50a523eb9398c51613b2a74693f07a50fe29"
         olson2014currbio_clean()
         error("Olson et al 2014 dataset download failed")
         return
