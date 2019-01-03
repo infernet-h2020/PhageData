262c790905e74e87f3066032c0c9a98bc218e77e
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2019-01-03 15:54:08 -0500

    cleanup

--- src/olson2014currbio.jl
+++ src/olson2014currbio.jl
@@ -67,12 +67,11 @@ end
 function olson2014currbio_download()
     run(`mkdir -p $olson2014currbio_dir`)
     @info "downloading Olson et al 2014 Current Biology dataset"
-    #url = "https://www.cell.com/cms/10.1016/j.cub.2014.09.072/attachment/3a36211d-bddd-43e3-bf42-a6721f93a18b/mmc2.xlsx"
     url = "https://ars.els-cdn.com/content/image/1-s2.0-S0960982214012688-mmc2.xlsx"
     file_path = olson2014currbio_dir * "/mmc2.xlsx"
     run(`curl -o $file_path $url`)
     if bytes2hex(open(sha2_256, file_path)) ≠ "e912c93cf3d1d95a7d8fc5bb8e8d50a523eb9398c51613b2a74693f07a50fe29"
-        #olson2014currbio_clean()
+        olson2014currbio_clean()
         error("Olson et al 2014 dataset download failed")
         return
     end
